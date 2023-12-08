e00ca0ddb923eaff3b32f68dd1a83d6b02945751
isha-c <61847743+isha-c@users.noreply.github.com>
2023-12-09 05:17:41 +0530

    Update python_task_1.py

--- templates/python_task_1.py
+++ templates/python_task_1.py
@@ -13,8 +13,9 @@ def generate_car_matrix(df)->pd.DataFrame:
                           where 'id_1' and 'id_2' are used as indices and columns respectively.
     """
     # Write your logic here
-
-    return df
+        car_matrix = pd.pivot_table(df, values='car', index='id_1', columns='id_2', fill_value=0)
+        car_matrix.values[[range(len(car_matrix))]*2] = 0
+    return car_matrix
 
 
 def get_type_count(df)->dict:
@@ -28,8 +29,12 @@ def get_type_count(df)->dict:
         dict: A dictionary with car types as keys and their counts as values.
     """
     # Write your logic here
+    df['car_type'] = pd.cut(df['car'], bins=[-float('inf'), 15, 25, float('inf')],
+                            labels=['low', 'medium', 'high'])
+    type_count = df['car_type'].value_counts().to_dict()
+  
 
-    return dict()
+    return type_count
 
 
 def get_bus_indexes(df)->list:
@@ -43,8 +48,10 @@ def get_bus_indexes(df)->list:
         list: List of indexes where 'bus' values exceed twice the mean.
     """
     # Write your logic here
+    bus_mean = df['bus'].mean()
+    bus_indexes = df[df['bus'] > 2 * bus_mean].index.tolist()
 
-    return list()
+    return sorted(bus_indexes)
 
 
 def filter_routes(df)->list:
@@ -58,8 +65,10 @@ def filter_routes(df)->list:
         list: List of route names with average 'truck' values greater than 7.
     """
     # Write your logic here
+    avg_truck_by_route = df.groupby('route')['truck'].mean()
+    selected_routes = avg_truck_by_route[avg_truck_by_route > 7].index.tolist()
 
-    return list()
+    return sorted(selected_routes)
 
 
 def multiply_matrix(matrix)->pd.DataFrame:
@@ -73,8 +82,9 @@ def multiply_matrix(matrix)->pd.DataFrame:
         pandas.DataFrame: Modified matrix with values multiplied based on custom conditions.
     """
     # Write your logic here
+     modified_matrix = car_matrix.applymap(lambda x: x * 0.75 if x > 20 else x * 1.25)
 
-    return matrix
+    return modified_matrix.round(1)
 
 
 def time_check(df)->pd.Series:
@@ -88,5 +98,14 @@ def time_check(df)->pd.Series:
         pd.Series: return a boolean series
     """
     # Write your logic here
+    df['timestamp'] = pd.to_datetime(df['timestamp'])
+    df['day_of_week'] = df['timestamp'].dt.dayofweek
+    df['hour'] = df['timestamp'].dt.hour
+    
+    time_check_result = (
+        df.groupby(['id', 'id_2'])['hour'].nunique() == 24) & (
+        df.groupby(['id', 'id_2'])['day_of_week'].nunique() == 7 )
+    
+    
 
-    return pd.Series()
+    return time_check_result
