dfdb7baeba3b199bdc25a0c9f5284d8d8251a164
isha-c <61847743+isha-c@users.noreply.github.com>
2023-12-10 04:32:00 +0530

    Update python_task_2.py

--- templates/python_task_2.py
+++ templates/python_task_2.py
@@ -12,8 +12,17 @@ def calculate_distance_matrix(df)->pd.DataFrame():
         pandas.DataFrame: Distance matrix
     """
     # Write your logic here
+    distance_matrix = pd.pivot_table(df, values='distance', index='id_start', columns='id_end', fill_value=0)
+    for col in distance_matrix.columns:
+        for row in distance_matrix.index:
+            if row != col:
+                route1 = distance_matrix.at[row, col]
+                route2 = distance_matrix.at[col, row]
+                total_distance = route1 + route2
+                distance_matrix.at[row, col] = distance_matrix.at[col, row] = total_distance
 
-    return df
+
+    return distance_matrix
 
 
 def unroll_distance_matrix(df)->pd.DataFrame():
@@ -27,8 +36,11 @@ def unroll_distance_matrix(df)->pd.DataFrame():
         pandas.DataFrame: Unrolled DataFrame containing columns 'id_start', 'id_end', and 'distance'.
     """
     # Write your logic here
+    unrolled_df = distance_matrix.stack().reset_index()
+    unrolled_df.columns = ['id_start', 'id_end', 'distance']
+    unrolled_df = unrolled_df[unrolled_df['id_start'] != unrolled_df['id_end']].reset_index(drop=True)
 
-    return df
+    return unrolled_df
 
 
 def find_ids_within_ten_percentage_threshold(df, reference_id)->pd.DataFrame():
@@ -44,8 +56,14 @@ def find_ids_within_ten_percentage_threshold(df, reference_id)->pd.DataFrame():
                           of the reference ID's average distance.
     """
     # Write your logic here
+    avg_distance_reference = df[df['id_start'] == reference_id]['distance'].mean()
+    threshold = 0.1 * avg_distance_reference
 
-    return df
+    ids_within_threshold = df.groupby('id_start')['distance'].mean()
+    ids_within_threshold = ids_within_threshold[ (avg_distance_reference - threshold <= ids_within_threshold) &
+        (ids_within_threshold <= avg_distance_reference + threshold)].reset_index()
+
+    return ids_within_threshold
 
 
 def calculate_toll_rate(df)->pd.DataFrame():
@@ -59,6 +77,17 @@ def calculate_toll_rate(df)->pd.DataFrame():
         pandas.DataFrame
     """
     # Wrie your logic here
+    toll_rates = {
+        'moto': 0.8,
+        'car': 1.2,
+        'rv': 1.5,
+        'bus': 2.2,
+        'truck': 3.6
+    }
+
+    for vehicle_type, rate in toll_rates.items():
+        df[vehicle_type] = df['distance'] * rate
+
 
     return df
 
@@ -74,5 +103,34 @@ def calculate_time_based_toll_rates(df)->pd.DataFrame():
         pandas.DataFrame
     """
     # Write your logic here
+    weekday_discounts = {
+        (0, 0, 0, 10, 0, 0): 0.8,
+        (10, 0, 0, 18, 0, 0): 1.2,
+        (18, 0, 0, 23, 59, 59): 0.8
+    }
+
+    weekend_discount = 0.7
+
+    df['start_day'] = df['end_day'] = df['start_time'] = df['end_time'] = None
+
+    for index, row in df.iterrows():
+        start_day, end_day = row['id_start'].split('_')[1], row['id_end'].split('_')[1]
+        start_time, end_time = map(int, row['id_start'].split('_')[0].split(':')), map(int, row['id_end'].split('_')[0].split(':'))
+        
+        df.at[index, 'start_day'] = start_day
+        df.at[index, 'end_day'] = end_day
+        df.at[index, 'start_time'] = start_time
+        df.at[index, 'end_time'] = end_time
+
+        is_weekend = start_day in ['Saturday', 'Sunday']
+        for time_range, discount_factor in weekday_discounts.items():
+            if not is_weekend and (
+                time_range[0] <= start_time[0] < time_range[3] or
+                (time_range[0] == start_time[0] and time_range[1] <= start_time[1] < time_range[4]) or
+                (time_range[1] == start_time[1] and time_range[2] <= start_time[2] <= time_range[5])):
+                df.at[index, 'car'] *= discount_factor
+
+        if is_weekend:
+            df.at[index, 'car'] *= weekend_discount
 
     return df
